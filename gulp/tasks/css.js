4f3f882d4b75d4a1f8069a96bb530dceaf4d4530
Randy Lubin <randylubin@gmail.com>
2015-03-23 16:05:28 -0700

    added 'to' for message and fixed sag

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -2,6 +2,7 @@
  * Create CSS file for use by the app.
  */
 
+var autoprefixer = require('gulp-autoprefixer');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
@@ -11,6 +12,7 @@ var config = require('../config').sass;
 gulp.task('css', function() {
   gulp.src(config.paths)
     .pipe(sass({includePaths: config.includePaths}))
+    .pipe(autoprefixer({ browsers: ['> 0.1%'] }))
     .pipe(rename(config.fileName))
     .pipe(gulp.dest(config.dest));
 });

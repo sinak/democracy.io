2cd62e13f0bfe1094c590dd00486d867155f37a8
Leah Jones <leah@l12s.com>
2015-03-23 15:05:12 -0700

    Wires up the ng-autocomplete directive, adds the gmaps script to the index template and tweaks the home controller to work with the autocomplete

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -3,21 +3,30 @@
  */
 
 
-
 var HomeController = function($scope, $location, dioApi, dioRepData) { 
 	
-	dioRepData.repList = []
+	dioRepData.repList = [];
 	dioRepData.repDataRecieved = false;
 
-	$scope.address = '';
+	$scope.data = {
+    address: '',
+    geocode: {}
+  };
+
+  $scope.ngAutocompleteOptions = {
+    country: 'US',
+    type: 'geocode',
+    watchEnter: true
+  };
 
 	$scope.submit = function(address){
 		dioRepData.repList = dioApi.getRepsByLocation(address);
 		dioRepData.repDataReceived = true;
-		for (var i=0 ; i < dioRepData.repList.length ; i++){
+
+    for (var i=0 ; i < dioRepData.repList.length ; i++){
 			dioRepData.repList[i].selected = true;
 		};
-		console.log(dioRepData.repList);
+
 		$location.path('/location'); //TODO add address param
 	};
 };

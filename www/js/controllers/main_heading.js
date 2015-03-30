5e1bb48a2385ea7fe7770a39941d9fba7cee0ac8
Randy Lubin <randylubin@gmail.com>
2015-03-30 14:48:47 -0700

    heading controller and updated message_form controller
    I added the heading controller that changes the data-pagename depending
    on the loaded partial.
    
    I updated the message_form controller to correctly grab the legislators

--- www/js/controllers/main_heading.js
+++ www/js/controllers/main_heading.js
@@ -2,20 +2,18 @@ var HeadingController = function($scope, $location) {
 
 	$scope.$on('$routeChangeSuccess', function(event, newLoc, oldLoc){
 		
-		if($location.path() === "/"){
-			$scope.pageName = "home";
+		if ($location.path() === '/'){
+			$scope.pageName = 'home';
 		} else {
 			$scope.pageName = $location.path();
 		}
 
-		if( $scope.pageName.charAt( 0 ) === '/' ){
-    		$scope.pageName = $scope.pageName.slice( 1 );
-    	}
+		if ($scope.pageName.charAt(0) === '/' ) {
+      $scope.pageName = $scope.pageName.slice(1);
+    }
 
 		var wrapper = document.querySelector('#wrapper');
-
 		wrapper.dataset.pagename = $scope.pageName;
-
 	});
 
 };

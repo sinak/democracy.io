18e180c1fce3355ff62efb3a0991be077443f02b
Leah Jones <leah@l12s.com>
2015-05-27 16:38:39 -0700

    Updates the CSS task to conditionally use minifyCSS

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -4,6 +4,8 @@
 
 var autoprefixer = require('gulp-autoprefixer');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var minifyCSS = require('gulp-minify-css');
 var path = require('path');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
@@ -11,9 +13,18 @@ var sass = require('gulp-sass');
 var config = require('../config');
 
 gulp.task('css', function() {
-  return gulp.src(path.join(config.WWW_DIR, 'sass/app.scss'))
-    .pipe(sass({includePaths: [config.NPM_DIR]}))
+  var stream = gulp.src(path.join(config.WWW_DIR, 'sass/app.scss'))
+    .pipe(sass({
+      includePaths: [config.NPM_DIR]
+    }))
     .pipe(autoprefixer({ browsers: ['> 0.1%'] }))
+
+  if (gutil.env.production) {
+    stream = stream
+      .pipe(minifyCSS())
+  }
+
+  return stream
     .pipe(rename('dio.min.css'))
     .pipe(gulp.dest(path.join(config.STATIC_DIR, 'css')));
 });

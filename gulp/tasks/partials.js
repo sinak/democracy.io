10cbd1430efa9cdc2e611bbc2b273b727bd0c3ce
Sina Khanifar <sina.khanifar@gmail.com>
2015-07-06 15:26:28 -0700

    Adds video to homepage

--- gulp/tasks/partials.js
+++ gulp/tasks/partials.js
@@ -5,8 +5,10 @@
 var gulp = require('gulp');
 var path = require('path');
 var templateCache = require('gulp-angular-templatecache');
+var replace = require('gulp-replace');
 
 var config = require('../config');
+var version = require('../../package.json').version;
 
 gulp.task('partials', function() {
   gulp.src(path.join(config.WWW_DIR, 'partials/**/*.html'))
@@ -15,5 +17,6 @@ gulp.task('partials', function() {
       module: 'democracyIoApp',
       root: '/partials'
     }))
+    .pipe(replace('{$VERSION}', version))
     .pipe(gulp.dest(path.join(config.BUILD_DIR, 'partials')));
 });

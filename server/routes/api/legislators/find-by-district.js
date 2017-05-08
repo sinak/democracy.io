909c17801f4c17f4154ed2ea96ab1064e5e23dd6
Sina Khanifar <sina.khanifar@gmail.com>
2017-05-07 20:25:02 -0700

    Fixes issue where no legislators returned if one member is not in congress.json data

--- server/routes/api/legislators/find-by-district.js
+++ server/routes/api/legislators/find-by-district.js
@@ -7,37 +7,39 @@ var Congress = require('../../../services/congress');
 var potc = require('../../../services/third-party-apis/potc');
 var resHelpers = require('../helpers/response');
 
+var Raven = require('../../../raven-client');
+
 var get = function (req, res) {
   var state = req.query.state,
       district = req.query.district;
 
-  if (!Congress.validDistrict(state, district))
-    res.status(400).json(resHelpers.makeError({ message: "Bad value for state/district parameter" }));
-  else {
-    var legislators = Congress.getLegislators(state, district);
-    var bioguideIds = legislators.map(function(legislator) {
-      return legislator.bioguideId;
-    });
-    
-    if (bioguideIds && legislators) {
-      // Call the PotC API get defunct and contact_url properties for each legislator
-      potc.getFormElementsForRepIdsFromPOTC(bioguideIds, req.app.locals.CONFIG, function(err, data) {
-        if (err) {
-          return res.status(400).json(resHelpers.makeError(err));
-        }
+  if (Congress.validDistrict(state, district) != true) {
+    Raven.captureMessage(Congress.validDistrict(state, district));
+  }
 
-        var augmentedLegislators = legislators.map(function(legislator) {
-          legislator.defunct = data[legislator.bioguideId].defunct || false;
-          legislator.contact_url = data[legislator.bioguideId].contact_url || null;
-          return legislator;
-        });
+  var legislators = Congress.getLegislators(state, district).filter(function(n){return n!= undefined});
+  var bioguideIds = legislators.map(function(legislator) {
+    return legislator.bioguideId;
+  });
+  
+  if (bioguideIds && legislators) {
+    // Call the PotC API get defunct and contact_url properties for each legislator
+    potc.getFormElementsForRepIdsFromPOTC(bioguideIds, req.app.locals.CONFIG, function(err, data) {
+      if (err) {
+        return res.status(400).json(resHelpers.makeError(err));
+      }
 
-        res.json(resHelpers.makeResponse(augmentedLegislators));
+      var augmentedLegislators = legislators.map(function(legislator) {
+        legislator.defunct = data[legislator.bioguideId].defunct || false;
+        legislator.contact_url = data[legislator.bioguideId].contact_url || null;
+        return legislator;
       });
-    }
-    else {
-      res.status(400).json(resHelpers.makeError({message: 'Missing legislator or bioguideID data.'}));
-    }
+
+      res.json(resHelpers.makeResponse(augmentedLegislators));
+    });
+  }
+  else {
+    res.status(400).json(resHelpers.makeError({message: 'Missing legislator or bioguideID data.'}));
   }
 };
 
